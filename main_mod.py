71e75f4d1e349c82568c972db8003945dc193e61
amoghthusoo <amoghthusoo@gmail.com>
2023-10-10 10:09:00 +0530

    Completed with 1218 patients

--- main_mod.py
+++ main_mod.py
@@ -151,7 +151,7 @@ def imToString(coordinates):
 	pytesseract.pytesseract.tesseract_cmd ='C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 	
 	cap = ImageGrab.grab(bbox = coordinates)  # (x_min, y_min, x_max, y_max)
-	cap = cv2.resize(nm.array(cap), None, fx=2.2, fy=2.2, interpolation=cv2.INTER_CUBIC)
+	cap = cv2.resize(nm.array(cap), None, fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
 	cap = cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY)
 	# cv2.imwrite("test.png", cap)
 	tesstr = pytesseract.image_to_string(cap, lang ='eng')
--- main_mod_2.py
+++ main_mod_2.py
@@ -7,8 +7,8 @@ from time import sleep
 import csv
 
 delay = 12
-offsetMode = False
-offsetValue = 6
+offsetMode = True
+offsetValue = 4
 newFile = False
 includeImage = True
 
@@ -152,7 +152,7 @@ def imToString(coordinates):
 	pytesseract.pytesseract.tesseract_cmd ='C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 	
 	cap = ImageGrab.grab(bbox = coordinates)  # (x_min, y_min, x_max, y_max)
-	cap = cv2.resize(nm.array(cap), None, fx=2.2, fy=2.2, interpolation=cv2.INTER_CUBIC)
+	cap = cv2.resize(nm.array(cap), None, fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
 	cap = cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY)
 	# cv2.imwrite("test.png", cap)
 	tesstr = pytesseract.image_to_string(cap, lang ='eng')
